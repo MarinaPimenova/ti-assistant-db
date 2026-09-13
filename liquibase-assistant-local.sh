fdbc549ba9497bb095f09ef9d94daa2779efbb2e
Marina Pimenova <mnfpma@gmail.com>
2026-09-13 17:05:17 +0300

    TI-25: Update liquibase scripts

--- liquibase-assistant-local.sh
+++ liquibase-assistant-local.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 
-# /mnt/c/Users/MarinaPimenova/sb-projects
-
 set -ex
 
-##echo "contextEnv: ${contextEnv}"
-##DB_ADDRESS=jdbc:postgresql://localhost:5434/assistant_db
-# url=jdbc:postgresql://host.docker.internal:5434/assistant_db
-# DB_ADDRESS=172.25.9.62
+DB_ADDRESS="${DB_ADDRESS:-ti-assistant-db:5432}"
 DB_PASSWORD=qwerty
 DB_username=assistant_user
 
-export PGPASSWORD="${DB_PASSWORD}"
 # psql -h ${DB_ADDRESS} -U ${DB_username} -d assistant_db -tc "CREATE SCHEMA IF NOT EXISTS assistant AUTHORIZATION assistant_user;"
  docker run --rm \
  --network knowledge-network \
  -v ./resources/liquibase:/liquibase/changelog \
- liquibase-pg:latest \
+  mnpma/liquibase-pg:5.0 \
  --url=jdbc:postgresql://ti-assistant-db:5432/assistant_db \
- --username=assistant_user \
- --password=qwerty \
+  --username=${DB_USERNAME} \
+  --password=${DB_PASSWORD} \
  --changeLogFile=liquibase-changelog.xml \
  --contexts=dev \
  update
--- liquibase-local.example.sh
+++ liquibase-local.example.sh
@@ -4,9 +4,6 @@
 
 set -ex
 
-##echo "contextEnv: ${contextEnv}"
-##DB_ADDRESS=jdbc:postgresql://localhost:5434/assistant_db
-# url=jdbc:postgresql://host.docker.internal:5434/assistant_db
 DB_ADDRESS=<see service name in docker-compose.yml>
 DB_PASSWORD=qwerty
 DB_username=assistant_user
@@ -14,9 +11,9 @@ DB_username=assistant_user
 export PGPASSWORD="${DB_PASSWORD}"
 # psql -h ${DB_ADDRESS} -U ${DB_username} -d assistant_db -tc "CREATE SCHEMA IF NOT EXISTS assistant AUTHORIZATION assistant_user;"
 docker run --rm -v ./resources/liquibase:/liquibase/changelog \
-  liquibase-pg:latest \
-  --url=jdbc:postgresql://${DB_ADDRESS}:5434/assistant_db \
-  --username=${DB_username} \
+  mnpma/liquibase-pg:5.0 \
+ --url=jdbc:postgresql://ti-assistant-db:5432/assistant_db \
+  --username=${DB_USERNAME} \
   --password=${DB_PASSWORD} \
   --changeLogFile=liquibase-changelog.xml \
   --contexts=dev \
